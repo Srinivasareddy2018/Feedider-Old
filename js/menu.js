c60d5ec2aef2e03a64bfdb861b9b66038e4016be
Srinivas <srinivasreddy3119@gmail.com>
2022-02-10 19:20:40 +0530

    Code Refactor & Added Menu Module

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+let menu = {
+    "hide_feed": {name: "Home Page Feed"},
+    "hide_home_page_left_sidebar": {name: "Home Left Sidebar"},
+    "hide_sidebar": {name: "Video Right Sidebar"},
+    "hide_comments": {name: "Comments"},
+    "hide_end_screen_feed": {name: "Video End Feed"},
+    "hide_end_screen_video_cards": {name: "Video End Cards"},
+    "hide_best_of_youtube": {name: "Best of Youtube"},
+    "hide_explore": {name: "Explore Button"},
+    "hide_subscriptions": {name: "Subscriptions Button"},
+    "hide_search_suggestions": {name: "Search Suggestions"},
+    "hide_people_also_watched": {name: "People also Watched"},
+    "hide_like_button": {name: "Like Button"},
+    "hide_dislike_button": {name: "Dislike Button"},
+    "hide_playlist": {name: "Hide Playlist"},
+    "hide_mixes" : {name: "Hide Mixes"},
+    "hide_autoplay": {name: "Disable Autoplay"},
+    "hide_description": {name: "Description"}
+}
+
+export default menu;
