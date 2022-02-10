bd91c01d8971ab42c89d643bd9af7e15d159af39
Srinivas <srinivasreddy3119@gmail.com>
2022-02-10 19:52:37 +0530

    Updated Menu Item Names

--- js/contentScript.js
+++ js/contentScript.js
@@ -2,23 +2,23 @@ console.log("Content Script is Running")
 const HTML = document.documentElement;
 
 let menu = {
-    "hide_feed": {name: "Home Page Feed"},
-    "hide_home_page_left_sidebar": {name: "Left Sidebar"},
-    "hide_sidebar": {name: "Video Right Sidebar"},
-    "hide_comments": {name: "Comments"},
-    "hide_end_screen_feed": {name: "Video End Feed"},
-    "hide_end_screen_video_cards": {name: "Video End Cards"},
-    "hide_best_of_youtube": {name: "Best of Youtube"},
-    "hide_explore": {name: "Explore Button"},
-    "hide_subscriptions": {name: "Subscriptions Button"},
-    "hide_search_suggestions": {name: "Search Suggestions"},
-    "hide_people_also_watched": {name: "People also Watched"},
-    "hide_like_button": {name: "Like Button"},
-    "hide_dislike_button": {name: "Dislike Button"},
+    "hide_feed": {name: "Hide Home Page Feed"},
+    "hide_home_page_left_sidebar": {name: "Hide Home Left Sidebar"},
+    "hide_sidebar": {name: "Hide Video Right Sidebar"},
+    "hide_comments": {name: "Hide Comments"},
+    "hide_end_screen_feed": {name: "Hide Video End Feed"},
+    "hide_end_screen_video_cards": {name: "Hide Video End Cards"},
+    "hide_best_of_youtube": {name: "Hide Best of Youtube"},
+    "hide_explore": {name: "Hide Explore Button"},
+    "hide_subscriptions": {name: "Hide Subscriptions Button"},
+    "hide_search_suggestions": {name: "Hide Search Suggestions"},
+    "hide_people_also_watched": {name: "Hide People also Watched"},
+    "hide_like_button": {name: "Hide Like Button"},
+    "hide_dislike_button": {name: "Hide Dislike Button"},
     "hide_playlist": {name: "Hide Playlist"},
     "hide_mixes" : {name: "Hide Mixes"},
     "hide_autoplay": {name: "Disable Autoplay"},
-    "hide_description": {name: "Description"}
+    "hide_description": {name: "Hide Video Description"}
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
