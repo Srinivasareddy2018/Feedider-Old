0357b524ab234f62a2e9c675972b364e94099896
Srinivas <srinivasreddy3119@gmail.com>
2022-02-10 19:42:20 +0530

    Revert "Code Refactor & Added Menu Module"
    This reverts commit c60d5ec2aef2e03a64bfdb861b9b66038e4016be.

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,7 +1,25 @@
-import menu from "./menu.js"
-
 console.log("Content Script is Running")
 const HTML = document.documentElement;
+
+let menu = {
+    "hide_feed": {name: "Home Page Feed"},
+    "hide_home_page_left_sidebar": {name: "Left Sidebar"},
+    "hide_sidebar": {name: "Video Right Sidebar"},
+    "hide_comments": {name: "Comments"},
+    "hide_end_screen_feed": {name: "Video End Feed"},
+    "hide_end_screen_video_cards": {name: "Video End Cards"},
+    "hide_best_of_youtube": {name: "Best of Youtube"},
+    "hide_explore": {name: "Explore Button"},
+    "hide_subscriptions": {name: "Subscriptions Button"},
+    "hide_search_suggestions": {name: "Search Suggestions"},
+    "hide_people_also_watched": {name: "People also Watched"},
+    "hide_like_button": {name: "Like Button"},
+    "hide_dislike_button": {name: "Dislike Button"},
+    "hide_playlist": {name: "Hide Playlist"},
+    "hide_mixes" : {name: "Hide Mixes"},
+    "hide_autoplay": {name: "Disable Autoplay"},
+    "hide_description": {name: "Description"}
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     HTML.setAttribute(request.attributeKey, request.attributeValue)
